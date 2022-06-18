525aa170143076d8bb9a9cc995d983596e96af03
Nikhil vimal <53228553+nik7860@users.noreply.github.com>
2022-06-18 17:26:35 +0530

    Update Navbar.jsx

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -17,9 +17,7 @@ export default function Navbar(props) {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/About">About</Link>
-        </li>
+        
         
         
       </ul>
