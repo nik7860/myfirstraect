4d373a4cffaad7ec7f8330b94570cd816eac04d9
Nikhil vimal <53228553+nik7860@users.noreply.github.com>
2022-06-18 20:35:20 +0530

    Update Textform.jsx

--- src/componants/Textform.jsx
+++ src/componants/Textform.jsx
@@ -40,10 +40,10 @@ export default function Textform(props) {
         <textarea className={`form-control bg-${props.mode} text-${props.color}`} value={text} onChange={handleUpChange} id="mybox" rows="8"></textarea>
 
       </div>
-      <button className='btn btn-primary mx-2' onClick={handleupclick}>Upper case </button>
-      <button className='btn btn-primary mx-2' onClick={handlelclick}>Lower case</button>
-      <button className='btn btn-primary mx-2' onClick={handlelclear}>Clear</button>
-      <button className='btn btn-primary mx-2' onClick={handlecopy}>Copy</button>
+      <button className='btn btn-primary mx-2 my-1' onClick={handleupclick}>Upper case </button>
+      <button className='btn btn-primary mx-2 my-1' onClick={handlelclick}>Lower case</button>
+      <button className='btn btn-primary mx-2 my-1' onClick={handlelclear}>Clear</button>
+      <button className='btn btn-primary mx-2 my-1' onClick={handlecopy}>Copy</button>
     </div><div className='container'>
         <h1>Your text summary!</h1>
         <p>{text.split(" ").filter((element)=>{return element,length!==0}).length}words and {text.length}characters</p>
