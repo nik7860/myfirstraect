809a4f58f42916daa8112807c5b6a2c2c490ad31
Nikhil vimal <53228553+nik7860@users.noreply.github.com>
2022-06-18 20:47:40 +0530

    Update Alert.js

--- src/componants/Alert.js
+++ src/componants/Alert.js
@@ -3,7 +3,7 @@ import propsTypes from 'prop-types';
 
 export default function Alert(props) {
   return (
-    <div>
+    <div style={{height:'50px'}}>
       <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{props.alert.type}</strong> : {props.alert.msg}
         
